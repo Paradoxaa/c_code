e80abfc53da72e54beb5077514985f1942bc5c6d
Your NameParadoxaa <1421181919@qq.com>
2026-04-30 19:29:09 +0800

    A Revert "new"
    This reverts commit 39ff9907ce28ef6a9be907ac65c274b3e16e5ffa.

--- git_learn/learn.cpp
+++ git_learn/learn.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-    int arr[3][3] = {
-        {1, 1, 1},
-        {1, 1, 1},
-        {1, 1, 1}
-    };
-
-    cout << "Hello, World!11" << endl;
+    std::cout << "Hello, World!11" << std::endl;
     return 0;
 }
-
