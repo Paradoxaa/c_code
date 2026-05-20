4d4b3231eee96fd9d0f8bee18d23629282428635
Your NameParadoxaa <1421181919@qq.com>
2026-05-20 16:57:22 +0800

    53 最大子数组和

---
+++ git_learn/leet12.cpp
@@ -0,0 +1,28 @@
+#include <iostream> //53 最大子数组和
+#include <sstream>
+#include <vector>
+#include <string>
+using namespace std;
+int main(){
+    vector<int> nums;
+    string line;
+    if(getline(cin,line)){
+        stringstream ss(line);
+        int a;
+        cout<<"shuchu";
+        while(ss>>a){
+            nums.push_back(a);
+        }
+    }
+
+    int sum=0, maxsum = INT_MIN;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] < nums[i]+sum){
+            sum += nums[i];
+        }else{
+            sum = nums[i];
+        }
+        maxsum = max(sum, maxsum);
+    }
+    cout<<maxsum<<endl;
+}
